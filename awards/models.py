ce5ed7690d9dd54851c846c39e3722f30802a7b4
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-12 22:41:38 +0300

    create our profile and project models

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    bio = models.TextField(null=True)
+    profile_photo = CloudinaryField('profile_photo', null=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+class Project(models.Model):
+    title = models.CharField(max_length = 60)
+    project_image = CloudinaryField('project_image', null=True)
+    description = models.TextField()
+    link = models.CharField(max_length = 200, null=True)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    prof_ref = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='projects', null=True)
+
+    class Meta:
+    
+        ordering = ['pub_date']
