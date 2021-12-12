0caf47be44bfa551388e21652855e4bc59477f92
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-12 22:40:11 +0300

    update our home view function

--- awards/views.py
+++ awards/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.http  import HttpResponse
 
+from awards.models import Project,User
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    projects = Project.objects.all()
+    users = User.objects.all()
+    return render(request, 'home.html', {"projects":projects, "users": users})
