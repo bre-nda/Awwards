868074756712114ebccf57bffdf7cfb1de30b17c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 16:45:51 +0300

    add the login,logout and register urls patterns

--- awards/urls.py
+++ awards/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('',views.home,name = 'home'),
@@ -8,4 +9,7 @@ urlpatterns=[
     path('profile/', views.profile, name='profile'),
     path('update/', views.update, name='update'),
     path('upload_project/', views.upload_project, name='upload_project'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
