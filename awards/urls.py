f40145d6146423465d3a28b5004a78f094225380
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 22:01:56 +0300

    add the rate and the project url patters

--- awards/urls.py
+++ awards/urls.py
@@ -12,4 +12,6 @@ urlpatterns=[
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('project/<project_id>/',views.project,name ='project'),
+    path('rate/<project_id>/',views.rate,name ='rate'),
 ]
