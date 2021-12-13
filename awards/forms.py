b7884fda032bae41e51790e8da1bc9fac84422e2
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 21:58:34 +0300

    add the rate form

--- awards/forms.py
+++ awards/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile, Project
+from .models import Profile, Project, Rating
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -30,3 +30,25 @@ class ProjectUploadForm(forms.ModelForm):
     def form_valid(self, form):
         form.instance.user = self.request.profile
         return super().form_valid(form)
+
+class RateForm(forms.ModelForm):
+    RATE_CHOICES = [
+    (10,'10-Outstanding'),
+    (9,'9-Exceeds Expectations'),
+    (8,'8-Excellent'),
+    (7,'7-Good'),
+    (6,'6-Barely Above Average'),
+    (5,'5-Average'),
+    (4,'4-Poor'),
+    (3,'3-Awful'),
+    (2,'2-Dreadful'),
+    (1,'1-Troll'),
+]
+
+    rate_design = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+    rate_usability = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+    rate_content = forms.ChoiceField(choices=RATE_CHOICES, widget = forms.Select(), required = True)
+
+    class Meta:
+        model = Rating
+        fields = ['rate_design', 'rate_usability', 'rate_content' ]
