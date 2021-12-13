e23173eb68ba998750a7646613cdd17cee64372c
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 15:01:50 +0300

    define the upload project function

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,11 @@
 from django.http.response import Http404
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 from django.http  import HttpResponse
-
+from django.core.exceptions import ObjectDoesNotExist
+from .forms import UserUpdateForm, ProfileUpdateForm, ProjectUploadForm
 from awards.models import Project,User
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -14,7 +16,7 @@ def home(request):
 def project(request,project_id):
     try:
         project = Project.objects.get(id = project_id)
-    except:
+    except ObjectDoesNotExist:
         raise Http404()
     return render(request,"project.html", {"project":project})
 
@@ -28,3 +30,39 @@ def search_results(request):
         message = "You haven't searched for any projects yet"
     return render(request, 'search.html', {'message': message})
 
+def profile(request):
+    projects = request.user.profile.projects.all()
+    return render(request, 'profile.html', {"projects":projects})
+
+def update(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+        instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Successfully updated your account!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'update.html', context)
+
+def upload_project(request):
+    users = User.objects.exclude(id=request.user.id)
+    if request.method == "POST":
+        form = ProjectUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit = False)
+            # project.prof_ref = request.user.profile
+            project.save()
+            messages.success(request, f'Successfully uploaded your Project!')
+            return redirect('index')
+    else:
+        form = ProjectUploadForm()
+    return render(request, 'upload_project.html', {"form": form, "users": users})
