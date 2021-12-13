53ec9fe36b4bb1c4919fe1514e96e5ef7dcda8d1
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 12:15:20 +0300

    add the search results url pattern

--- awards/urls.py
+++ awards/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns=[
     path('',views.home,name = 'home'),
     path('project/<project_id>/',views.project,name ='project'),
+    path('search/', views.search_results, name='search_results'),
 ]
