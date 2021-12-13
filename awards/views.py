ec07d8c23a0b7be47936b47354cabe8ef54eb446
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 11:59:28 +0300

    define project function

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 
 from django.http  import HttpResponse
@@ -9,3 +10,10 @@ def home(request):
     projects = Project.objects.all()
     users = User.objects.all()
     return render(request, 'home.html', {"projects":projects, "users": users})
+
+def project(request,project_id):
+    try:
+        project = Project.objects.get(id = project_id)
+    except:
+        raise Http404()
+    return render(request,"project.html", {"project":project})
