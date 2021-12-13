e994b3c528c52a12671f3471b11eba7750b18020
bre-nda <brenda.kanale@student.moringaschool.com>
2021-12-13 15:05:31 +0300

    add the update profile and update project url pattern

--- awards/urls.py
+++ awards/urls.py
@@ -5,4 +5,7 @@ urlpatterns=[
     path('',views.home,name = 'home'),
     path('project/<project_id>/',views.project,name ='project'),
     path('search/', views.search_results, name='search_results'),
+    path('profile/', views.profile, name='profile'),
+    path('update/', views.update, name='update'),
+    path('upload_project/', views.upload_project, name='upload_project'),
 ]
